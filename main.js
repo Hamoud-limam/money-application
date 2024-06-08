118eb2163a9b4049f618387fb5f58da2d995ba86
Hamoud-limam <hamoudlimam01@gmail.com>
2024-06-08 13:13:52 +0000

    Add files via upload
    login page

---
+++ main.js
@@ -0,0 +1,17 @@
+function compareValue(){
+let inputValue = document.getElementById('inputValue').value;
+if(inputValue ==='36515734'){
+  alert('right name');
+
+ 
+}
+else{
+  alert('wrong name');}
+  let inputPass = document.getElementById('inputPass').value;
+  if (inputPass === '1234') {
+    alert('right pass');
+  }
+  else {
+    alert('wrong pass');
+  }
+}
